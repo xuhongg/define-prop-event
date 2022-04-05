08713497403e18509ce210596254eec8e34149d4
xuhongg <40853948+xuhongg@users.noreply.github.com>
2022-04-05 19:36:57 +0800

    Add files via upload
    base on vant-vue-master/vue/vue2

---
+++ src/view/testModel/BaseBox.vue
@@ -0,0 +1,33 @@
+
+<template>
+    <div>
+        <input type="text" :value="childName" @input="updateVal($event.target.value)" />
+    </div>
+</template>
+
+
+<script>
+export default {
+    name: "BaseBox",
+    model: {
+        prop: 'childName',//父组件v-model去绑定的变量 
+        event: 'changeX'  // 随便命名事件，对应下面$emit即可。demo是在输入框输入值后，触发emit去通知父组件更新 
+    },
+    props: {
+        uname: {
+            type: String,
+            default: '子组件定义初始值'//会被父组件的绑定值给覆盖掉
+        }
+    },
+    methods: {
+        //val就是更新后的值 通知父组件更新
+        updateVal(val) {
+            this.$emit('changeX', val)
+        }
+    }
+}
+</script>
+
+<style scoped>
+</style>
+
---
+++ src/view/testModel/HomeView.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="homeView">
+    <BaseBox v-model="name" value="some value" ></BaseBox>
+    父组件name:{{name}}
+  </div>
+</template>
+
+<script>
+//引入子组件
+import BaseBox from './BaseBox'
+
+export default {
+  name: "HomeView",
+  //注册子组件
+  components: {
+    BaseBox,
+  },
+  data() {
+    return {
+      //定义v-model属性初值
+      name: '父组件定义初始值'
+    }
+  },
+  created() {
+    this.$nextTick(() => {
+    
+    })
+  },
+  methods:{
+  
+  }
+
+}
+</script>
+
+<style scoped>
+.homeView {
+  width: 80%;
+  height: auto;
+  margin: auto;
+}
+</style>
+
